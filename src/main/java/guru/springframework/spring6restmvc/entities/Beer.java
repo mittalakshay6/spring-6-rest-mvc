7819bdb31d24fe35e1013a49541b1182f66ce654
Akshay Mittal <akshay.m@turvo.com>
2023-08-27 12:00:27 +0530

    add column length validation on beer name at db level

--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -38,6 +39,8 @@ public class Beer {
     private Integer version;
 
     @NotBlank
+    @Column(length = 15)
+    @Size(max = 15)
     private String beerName;
 
     @NotNull
--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
@@ -28,5 +30,18 @@ class BeerRepositoryTest {
         beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
+    }
+
+    @Test
+    void saveBeerTestNameTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+             beerRepository.save(Beer.builder()
+                    .beerName("My beer - more than 15 letters beer name")
+                    .beerStyle(BeerStyle.IPA)
+                    .upc(UUID.randomUUID().toString())
+                    .price(BigDecimal.TEN)
+                    .build());
+            beerRepository.flush();
+        });
     }
 }
